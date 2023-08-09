83a9efe55b1f04df30a76e0a2ca5076b97c69763
Zachary Kuchar <zacharyk323@live.com>
2023-08-09 16:33:49 -0400

    Audio plays at start each time

--- app.js
+++ app.js
@@ -2,5 +2,6 @@ window.addEventListener("keydown", (e) => {
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
   if (!audio) return;
   console.log(audio);
+  audio.currentTime = 0; //Rewind audio to start
   audio.play();
 });
