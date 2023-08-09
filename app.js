fce538ec1dd872addad1fb67689b20bd8a725bd0
Zachary Kuchar <zacharyk323@live.com>
2023-08-09 18:33:31 -0400

    Modify key class

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 window.addEventListener("keydown", (e) => {
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
   if (!audio) return;
-  console.log(audio);
+  //   console.log(audio);
   audio.currentTime = 0; //Rewind audio to start
   audio.play();
+  console.log(key);
+  key.classList.add("playing");
 });
