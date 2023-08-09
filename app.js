513f2af34a99ddd2ded9f2b40c366c8b94a0e13e
Zachary Kuchar <zacharyk323@live.com>
2023-08-09 18:50:23 -0400

    Moved transform function

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-window.addEventListener("keydown", (e) => {
+const transform = (e) => {
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
   const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
   if (!audio) return;
@@ -7,7 +7,9 @@ window.addEventListener("keydown", (e) => {
   audio.play();
   //   consaole.log(key);
   key.classList.add("playing");
-});
+};
+
+window.addEventListener("keydown", transform);
 
 const keys = document.querySelectorAll(".key");
 
