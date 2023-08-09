bf26b6b9ae4c30dfd79af4db1f9a2f4c4ac9ebb2
Zachary Kuchar <zacharyk323@live.com>
2023-08-09 16:17:32 -0400

    Can now play audio ques

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 window.addEventListener("keydown", (e) => {
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  if (!audio) return;
   console.log(audio);
+  audio.play();
 });
